c185ca2d9c31d4f219ee03579da9bd2d1a5ac6e0
Mahmoud Tolba <78425511+Mahmoud-t0lba@users.noreply.github.com>
2022-07-31 19:04:54 +0200

    x

--- lib/models/change_favorites_model.dart
+++
@@ -1,9 +0,0 @@
-class ChangeFavoritesModel {
-  bool status;
-  String message;
-
-  ChangeFavoritesModel.fromJson(Map<String, dynamic> json) {
-    status = json['status'];
-    message = json['message'];
-  }
-}
